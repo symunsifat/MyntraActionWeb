6ff87ffc9d470b1b1aa34edd5c1943202736d140
FarzeenHasan <farzeen.hasan18@gmail.com>
2021-08-03 21:52:15 -0400

    edited by farzeen

--- MyntraProject/src/test/java/Elements/MyntraWebElements.java
+++ MyntraProject/src/test/java/Elements/MyntraWebElements.java
@@ -1,5 +1,31 @@
 package Elements;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyntraWebElements {
+	
+
+@FindBy(xpath="//*[@class='myntraweb-sprite desktop-logo sprites-headerLogo']")
+public WebElement mainLogo;
+
+@FindBy(xpath="//*[@id=\"mountRoot\"]/div/div[1]/main/div/div[6]/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/a/picture/img']")
+public WebElement gapImage;
+
+@FindBy(xpath="//*[@class=\"myntraweb-sprite desktop-iconBag sprites-headerBag\']")
+public WebElement addBag;
+
+@FindBy(xpath="//*[@class=\'desktop-searchBar']")
+public WebElement searchBar;
+
+
+ 
+
+
+
+
+
+
+
 
 }
